bea632cd42ae434a647954901623dedf8182a388
Sean Brown <sbbrown35@gmail.com>
2024-01-03 10:17:00 -0500

    sdf

--- seeds/seed_season_ag.py
+++ seeds/seed_season_ag.py
@@ -3,24 +3,20 @@
 from database import db
 from sqlalchemy import func
 from models.models import SeasonAggregation, History
-from utils.loaders import print_progress_dots  
+from utils.loaders import print_progress_dots
 
 
-def seed_season_ag():
-    print("BEGIN SEEDING AGGREGATED SEASON DATA")
-    print_progress_dots(12)
+def flatten_season_data():
+    all_data = []
     
     subquery = (
-        db.query(func.min(History.id).label("min_id")).group_by(History.season, History.club_id).subquery())
+    db.query(func.min(History.id).label("min_id")).group_by(History.season, History.club_id).subquery())
 
     unique_rows_by_season = (db.query(History).join(subquery, History.id == subquery.c.min_id).all())
-
   
     for history_season_row in unique_rows_by_season:
         season = history_season_row.season 
         
-        print('unique', history_season_row.offensive_team_plays )
-
         total_offensive_team_plays = sum(row.offensive_team_plays or 0 for row in unique_rows_by_season if row.season == season)
         total_defensive_team_plays = sum(row.defensive_team_plays or 0 for row in unique_rows_by_season if row.season == season)
 
@@ -30,10 +26,25 @@ def seed_season_ag():
             total_offensive_team_plays=total_offensive_team_plays,
             total_defensive_team_plays=total_defensive_team_plays
         )
+        all_data.append(season_aggregation)
+        
+    return all_data
 
-        db.add(season_aggregation)
+def seed_season_ag():
+    print("BEGIN SEEDING AGGREGATED SEASON DATA")
+    print_progress_dots(12)
+    
+    data = flatten_season_data()
+    
+    print("LOOP and return dict")
+    
+    # for season_data in data:
+    #     print
 
-    db.commit()
+
+    #     db.add(season_aggregation)
+
+    # db.commit()
 
 if __name__ == "__main__":
     seed_season_ag()
